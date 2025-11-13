60c0d0015558736f160ff99ac60e97a52f8d53d0
Ahmed-528 <ahmaedmoh528@gmail.com>
2025-11-13 21:54:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 const int MAX_SALES = 7;
 double sales[MAX_SALES];
+string days[MAX_SALES] = {"Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"};
 
 void menu()
 {
     cout << "\033[94m";
     cout << "===================================" << endl;
-    cout << "\t Sales Tracket Menu" << endl;
+    cout << "\t Sales Tracker Menu" << endl;
     cout << "===================================" << endl;
     cout << "1. Enter daily sales data\n";
     cout << "2. Display all sales\n";
@@ -16,7 +17,7 @@ void menu()
     cout << "4. Calculate average sales\n";
     cout << "5. Show highest sales day\n";
     cout << "6. Show lowest sales day\n";
-    cout << "7. Searsh for a sales value\n";
+    cout << "7. Search for a day\n";
     cout << "8. Sort sales\n";
     cout << "9. Save data to file\n";
     cout << "10. help\n";
@@ -36,12 +37,27 @@ void inputSales()
     cout << "\nSales data stored in the array successfully.\n\n\n";
 }
 
-int search(string search_day) {
-	for(int i= 0 ; i< MAX_SALES ;i++) {
-		if(search_day==days[i]) {
-			return i;
+int find_max_sales(double sales[]) {
+	int max= 0; // Assume the first day has the highest sales.
+	
+	for(int i=1; i< MAX_SALES ;i++) {
+		if(sales[i]>sales[max]) {
+			max= i; //  Save the index of the highest sales day.
 		}
 	}
+	  return max; // Return the index of the highest day.
+}
+
+int search(string search_day)
+{
+    for (int i = 0; i < MAX_SALES; i++)
+    {
+        if (search_day == days[i])
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main()
@@ -74,7 +90,8 @@ int main()
             cout << "\nYou chose to show the lowest sales day.\n\n";
             break;
         case 7:
-            cout << "\nYou chose to searsh for a sales value.\n\n";
+            cout << "\nYou chose to searsh for a day.\n\n";
+            
             break;
         case 8:
             cout << "\nYou chose to sort sales.\n\n";
