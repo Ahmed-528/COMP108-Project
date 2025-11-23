f46cb30cb59c2d68788130d7cfd12a03c47b19a0
Ahmed-528 <ahmaedmoh528@gmail.com>
2025-11-23 13:24:42 +0300

    اضافة دالة Help

--- main.cpp
+++ main.cpp
@@ -81,36 +81,36 @@ double average() //This function is to calculate the average of the week.
 
 int find_max_sales() //This function finds the highest sales day.
 {
-    int max = 0; // Assume the first day has the highest sales.
+    int max = 0; 
 
     for (int i = 1; i < MAX_SALES; i++)
     {
         if (sales[i] > sales[max])
         {
-            max = i; // Save the index of the highest sales day.
+            max = i; 
         }
     }
 
     cout << "\033[32mHighest sales is on Day " << (max + 1) << " with amount: " << sales[max] << "\033[32m\n\n\n";
 
-    return max; // Return the index of the highest day.
+    return max; 
 }
 
 int find_lowest_sales() //This function finds the lowest sales day.
 {
-    int lowest = 0; // Assume the first day has the lowest sales.
+    int lowest = 0; 
 
     for (int i = 1; i < MAX_SALES; i++)
     {
         if (sales[i] < sales[lowest])
         {
-            lowest = i; // Save the index of the lowest sales day.
+            lowest = i; 
         }
     }
 
     cout << "\033[31mLowest sales is on Day " << (lowest + 1) << " with amount: " << sales[lowest] << "\033[32m\n\n\n";
 
-    return lowest; // Return the index of the lowest day.
+    return lowest; 
 }
 
 int search_day() //This function finds the day that the user is searching for.
@@ -168,6 +168,30 @@ void sortArray() //This function sorts sales in ascending order.
 
 }
 
+void showHelp()
+{
+    cout<<"\n==================================="<<endl;
+    cout<<"\t    HELP SECTION"<<endl;
+    cout<<"==================================="<<endl;
+    cout<<"This program helps you track and manage daily sales data.\n\n";
+    
+    cout<<"MENU OPTIONS EXPLAINED:\n";
+    cout<<"1. Enter daily sales data - Add new sales figures for different days\n";
+    cout<<"2. Display all sales - Show all the sales data you have entered\n";
+    cout<<"3. Calculate total sales - Add up all sales to get the sum\n";
+    cout<<"4. Calculate average sales - Find the average of all sales\n";
+    cout<<"5. Show highest sales day - Find the day with the most sales\n";
+    cout<<"6. Show lowest sales day - Find the day with the least sales\n";
+    cout<<"7. Search for a sales value - Look for a specific sales amount\n";
+    cout<<"8. Sort sales - Arrange sales data in order (ascending/descending)\n";
+    cout<<"9. Save data to file - Store your sales data in a file\n";
+    cout<<"10. Help - Show this help information\n";
+    cout<<"11. Exit program - Close the program\n\n";
+    
+    cout<<"Simply choose a number from the menu to perform that action.\n";
+    cout<<"After each action, the menu will appear again until you choose to exit.\n";
+    cout<<"===================================\n\n";
+}
 
 int main()
 {
@@ -218,7 +242,7 @@ int main()
             cout << "\nYou chose to save data to file.\n\n";
             break;
         case 10:
-
+            showHelp();
             break;
         case 11:
 
