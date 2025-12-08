8350d73b075aa39dd17c117e16e6a315efd2fc73
Ahmed-528 <ahmaedmoh528@gmail.com>
2025-12-08 20:59:53 +0300

    Create SortingOptions.cpp

---
+++ SortingOptions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void SortingOptions(){
+
+int numberChoice;
+
+  cout<<"Enter 1 for ascending sort or 2 for descending sort: \n";
+  cin>>numberChoice;
+
+  if(numberChoice == 1){
+    AscendingSort();
+  }
+  else if(numberChoice == 2){
+    DescendingSort();
+  }
+  else
+    cout<<"The entered value is not acceptable.\n";
+ 
+}
