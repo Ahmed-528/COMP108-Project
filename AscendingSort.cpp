7a1389a58961c7a4f29354321aff55b51957303d
HassanVTR <assanvtr@gmail.com>
2025-12-08 21:24:46 +0300

    Add DailySales struct and sales arrays

--- AscendingSort.cpp
+++ AscendingSort.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+struct DailySales
+{
+    string dayName;
+    double sales;
+};
+
 const int MAX_SALES = 7;
 double sales[MAX_SALES];
 double sortsales[MAX_SALES];
