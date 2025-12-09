84f7c1471fd23b2fb57bd0800ac60668610bbe5a
Ahmed-528 <ahmaedmoh528@gmail.com>
2025-12-09 18:46:05 +0300

    Update exit.cpp

--- exit.cpp
+++ exit.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
-int main () {
 
-    return 0;
+void exit()
+{
+    char choice;
+    while (true)
+    {
+        cout << "\nYou have unsaved changes!" << endl;
+        cout << "Enter 'y' to exit with saving. Enter 'n' to exit without saving.\n";
+        cin >> choice;
+
+        if (choice == 'y' || choice == 'Y')
+        {
+            saveData();
+            cout << "Exiting program." << endl;
+            break;
+        }
+        else if (choice == 'n' || choice == 'N')
+        {
+            cout << "Exiting without saving." << endl;
+            break;
+        }
+        else
+        {
+            cout << "Invalid input. Please enter 'y' or 'n'." << endl;
+        }
+    }
 }
