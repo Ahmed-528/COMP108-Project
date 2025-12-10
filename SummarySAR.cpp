c530881b4a8ca2395603e8c384ed9500945f4e2a
Hussain851t <hghazwi851@gmail.com>
2025-12-10 22:36:09 +0300

    Add files via upload

---
+++ SummarySAR.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void SummarySAR() {
+	setupDataSAR;
+	menuSAR;
+	inputSalesSAR;
+	displayArraySAR;
+	sumSAR;
+	averageSAR;
+	findMaxSalesSAR;
+	findLowestSalesSAR;
+	findOptionsSAR;
+	searchByDaySAR;
+	SearchByValueSAR;
+	searchOptionsSAR;
+	ascendingSortSAR;
+	descendingSortSAR;
+	SortingOptionsSAR;
+	updateDaySalesSAR;
+	convertSARtoUSD;
+	saveSalesSAR;
+	exitSAR;
+	mainSAR;
+}
---
+++ SummaryUSD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void SummaryUSD() {
+	setupDataUSD;
+	menuUSD;
+	inputSalesUSD;
+	displayArrayUSD;
+	sumUSD;
+	averageUSD;
+	findMaxSalesUSD;
+	findLowestSalesUSD;
+	findOptionsUSD;
+	searchByDayUSD;
+	SearchByValueUSD;
+	searchOptionsUSD;
+	ascendingSortUSD;
+	descendingSortUSD;
+	SortingOptionsUSD;
+	updateDaySalesUSD;
+	convertUSDtoSAR;
+	saveSalesUSD;
+	exitUSD;
+	mainUSD;
+}
