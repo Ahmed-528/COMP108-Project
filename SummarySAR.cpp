4a6f4f27d9a416cc6c07ef036e59018820212775
Hussain851t <hghazwi851@gmail.com>
2025-12-12 14:41:45 +0300

    Update SummarySAR.cpp

--- SummarySAR.cpp
+++ SummarySAR.cpp
@@ -2,24 +2,12 @@
 using namespace std;
 
 void SummarySAR() {
-	setupDataSAR;
-	menuSAR;
-	inputSalesSAR;
 	displayArraySAR;
 	sumSAR;
 	averageSAR;
 	findMaxSalesSAR;
 	findLowestSalesSAR;
-	findOptionsSAR;
-	searchByDaySAR;
-	SearchByValueSAR;
-	searchOptionsSAR;
 	ascendingSortSAR;
 	descendingSortSAR;
-	SortingOptionsSAR;
-	updateDaySalesSAR;
 	convertSARtoUSD;
-	saveSalesSAR;
-	exitSAR;
-	mainSAR;
 }
