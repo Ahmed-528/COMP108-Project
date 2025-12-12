cf7d587bafcba1a12d164d125bb98893f723f878
Hussain851t <hghazwi851@gmail.com>
2025-12-12 14:43:48 +0300

    Update SummaryUSD.cpp

--- SummaryUSD.cpp
+++ SummaryUSD.cpp
@@ -2,24 +2,12 @@
 using namespace std;
 
 void SummaryUSD() {
-	setupDataUSD;
-	menuUSD;
-	inputSalesUSD;
 	displayArrayUSD;
 	sumUSD;
 	averageUSD;
 	findMaxSalesUSD;
 	findLowestSalesUSD;
-	findOptionsUSD;
-	searchByDayUSD;
-	SearchByValueUSD;
-	searchOptionsUSD;
 	ascendingSortUSD;
 	descendingSortUSD;
-	SortingOptionsUSD;
-	updateDaySalesUSD;
 	convertUSDtoSAR;
-	saveSalesUSD;
-	exitUSD;
-	mainUSD;
 }
